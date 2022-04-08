d8e4e17b8b5797405d28ac06078cc9b63caa07ec
jusbuckingham <jus.buckingham@gmail.com>
2022-04-08 12:05:44 -0700

    completed again

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0x9821Ec8B576b3174c0dAa87B3cFa2e79bD4fBB47";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "nft-collection-m0o050o48-jusbuckingham.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
